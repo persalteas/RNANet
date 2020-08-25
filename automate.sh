be756956cfe79321fa506e22a34d9287fc165f7b
Louis Becquey <louis.becquey@univ-evry.fr>
2020-08-25 14:53:23 +0000

    automation details

--- automate.sh
+++ automate.sh
@@ -4,7 +4,7 @@ cd /home/lbecquey/Projects/RNANet;
 rm -f stdout.txt stderr.txt errors.txt;
 
 # Run RNANet
-time './RNAnet.py --3d-folder /home/lbequey/Data/RNA/3D/ --seq-folder /home/lbecquey/Data/RNA/sequences/ -s -r 20.0 --archive' > stdout.txt 2> stderr.txt;
+nohup bash -c 'time ./RNAnet.py --3d-folder /home/lbecquey/Data/RNA/3D/ --seq-folder /home/lbecquey/Data/RNA/sequences/ -s -r 20.0 --archive';
 
 # Compress
 rm -f results/RNANet.db.gz
